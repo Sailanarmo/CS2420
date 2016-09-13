466042755ec75ce6f07d9ce6153a82dd773289e2
Raul Ramirez <rlramirez12@gmail.com>
2016-09-13 15:24:48 -0600

    Linked list created

---
+++ Word Ladder Puzzle/game.hpp
@@ -0,0 +1,18 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <random>
+
+#include "linkedlist.hpp"
+
+
+class Game{
+
+    Game(std::string file) {dic.reserve(355000); 
+
+
+#endif
--- Word Ladder Puzzle/linkedList.hpp
+++ Word Ladder Puzzle/linkedList.hpp
@@ -2,7 +2,7 @@
 #define LINKED_LIST_HPP
 
 #include <memory>
-
+#include <sstream>
 
 template <typename T>
 struct Node{
@@ -10,46 +10,122 @@ struct Node{
     T data;
     std::shared_ptr<Node> prev;
     std::shared_ptr<Node> next;
-    Node(T t, std::shared_ptr<Node> list = nullptr) : data(t), prev(list), next(list){}
+    Node(T t, std::shared_ptr<Node> p = nullptr, std::shared_ptr<Node> n = nullptr) : data(t), prev(p), next(n){}
 };
-
-template <typename T>
-void insertN(std::shared_ptr<Node<T>> &list, T data){
-
-    if(!list){
-	list = std::make_shared<Node<T>>(data);
-    } else if(list->data > data){
-	auto temp = std::make_shared<Node<T>>(data, list);
-    } else {
-	insertN(list->next, data);
-    }
-}
-
-template <typename T>
-void removeN(std::shared_ptr<Node<T>> &list, T target){
-
-    if(!list) return;
-    if(target == list->data){
-	list = list->next;
-    } else {
-	removeN(list->next, target);
-    }
-}
 
 template <typename T>
 class LinkedList{
 
 public:
     
-    LinkedList();
+    LinkedList() : head(nullptr) {}
 
+    void pushBack(T);
+    void pushFront(T);
+    T popOff();
+    T pushOff();
+    void clear() { recurClear(head) : head = tail = nullptr; };
+    std::string toString();
+    int sizeOf(){ return sizeOfRecur(head); }
 
 private: 
 
     std::shared_ptr<Node<T>> head;
     std::shared_ptr<Node<T>> prev;
 
+    void recurClear(std::shared_ptr<Node> curr);
 };
+
+template <template T>
+void LinkedList<T>::pushBack(T t){
+ 
+    if(!head){
+	 head = tail = std::make_shared<Node>(d);
+	 return;
+    }
+
+    tail->next = std::make_shared<Node>(d, nullptr, tail);
+    tail = tail->next;
+}
+
+template <template T>
+void LinkedList<T>::pushFront(T t){
+
+    if(!head){
+	head = tail = std::make_shared<Node>(d);
+	return;
+    }
+
+    head->prev = std::make_shared<Node>(d, head, nullptr);
+    head = head->prev;
+}
+
+template <template T>
+T LinkedList<T>::popOff(){
+
+    if(!head){
+	std::cout << "Out of Range, aborting!!" << std::endl;
+	return;
+    }
+
+    auto temp = head;
+    if(head->next){
+	head = tail = nullptr;
+	return temp->data;
+    }
+
+    head = head->next;
+    head->prev = nullptr;
+    return temp ->data;
+}
+
+template <template T>
+T LinkedList<T>::pushOff(){
+
+    if(!tail){
+	std::cout << "The list is empty, aborting!! " << std::endl;
+        return;
+    }
+
+    tail->data;
+}
+
+template <template T>
+void LinkedList<T>::recurClear(std::shared_ptr<Node> curr){
+
+    if(!curr){
+	return;
+    }
+    
+    curr->prev = nullptr;
+    recurClear(curr->next);
+}
+
+template <template T>
+std::string LinkedList<T>::toString(){
+
+    std::stringstream ss;
+
+    for(auto curr = head; curr != nullptr; curr=curr->next){
+	ss << curr->data << " ";
+    }
+
+    ss << std::endl;
+
+    return ss.str();
+}
+
+template <template T>
+int LinkedList<T>::sizeOfRecur(std::shared_ptr<Node> curr){
+
+    if(!curr){
+	return;
+    }
+
+    return 1 + sizeOfRecur(curr->next);
+}
+
+
 
 
 #endif
